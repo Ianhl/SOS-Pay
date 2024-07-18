7cde34991481f03f55a12380a702ddaf369a368e
Ianhl <larteyian@gmail.com>
2024-07-18 07:44:04 +0000

    First save

---
+++ core/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import redirect, render, get_object_or_404
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from . import forms
+from django.contrib import messages
+from django.conf import settings
+from .models import Payment
+from pypaystack import Transaction, Customer, Plan
+
+# Create your views here.
+
+def initiate_payment(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        payment_form = forms.PaymentForm(request.POST)
+        if payment_form.is_valid():
+            payment = payment_form.save()
+            return render(request, 'make_payment.html', {'payment': payment, 'paystack_public_key': settings.PAYSTACK_PUBLIC_KEY})
+    else:
+        payment_form = forms.PaymentForm()
+    return render(request, 'initiate_payment.html', {'payment_form': payment_form})
+
+
+def verify_payment(request: HttpRequest, ref: str) -> HttpResponse:
+    payment = get_object_or_404(Payment, ref=ref)
+    verified = payment.verify_payment()
+    if verified:
+        messages.success(request, "Verification S   uccessful")
+    else:
+       return redirect('initiate-payment')
+
+
+# def verify(request, id):
+#     transaction = Transaction(authorization_key= settings.PAYSTACK_SECRET_KEY)
+#     response = transaction.verify(id)
+#     data = JsonResponse(response, safe=False)
+#     return data
