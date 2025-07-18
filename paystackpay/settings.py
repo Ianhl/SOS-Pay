7cb55ee6ddec2fa3da778f66951190eb17b87f9e
Ian <Larteyian@gmail.com>
2025-07-18 14:20:16 +0000

    .

--- paystackpay/settings.py
+++ paystackpay/settings.py
@@ -36,7 +36,8 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'bnjg eojt oake yjcn'
 EMAIL_PORT = '587'
 
 
